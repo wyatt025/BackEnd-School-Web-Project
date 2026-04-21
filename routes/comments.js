dae6da7d3a7b5d0090d187b2a44e29501f07465a
wyatt025 <wnickerso@gmail.com>
2026-04-21 09:48:12 +0300

    testing comment permissions

--- routes/comments.js
+++ routes/comments.js
@@ -21,13 +21,13 @@ commentRouter.get("/:videoId", async (req, res) => {
 
 // POST comment
 commentRouter.post("/", async (req, res) => {
- /*   if (req.body.user_name === null) {
+    if (req.body.user_name === null) {
         return res.status(403).json({ message: "Please log in or sign up to add a comment" });
-    }*/
+    }
     try {
         const { video_id, user_name, content } = req.body;
         await comments.postComment(video_id, user_name, content);
-    res.sendStatus(201);
+        res.sendStatus(201);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: error.message });
