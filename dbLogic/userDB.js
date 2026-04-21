bc6ddb1ef3bd809006396b844014e119d214a45e
wyatt025 <wnickerso@gmail.com>
2026-04-21 10:47:52 +0300

    Moved backend url to the env

--- dbLogic/userDB.js
+++ dbLogic/userDB.js
@@ -9,7 +9,8 @@ const openDB = () => {
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
-        ssl: process.env.SSL
+        ssl: process.env.SSL,
+        backendURL: process.env.BACKEND_URL
     });
     return pool;
 };
